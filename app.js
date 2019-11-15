30227d1cf52691fac95b3f9a6e7ea7f354840d2d
Ho Trung Nhan <hotrungnhan29@gmail.com>
2019-11-16 05:45:38 +0700

    Controller and services

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const session = require('express-session');
-
+const bodyParser = require('body-parser');
 require('dotenv/config');
 require('./config/passport');
 
+//route
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
 const signupRouter = require('./routes/signup');
@@ -22,7 +23,7 @@ const adminRouter = require('./routes/admin');
 const blogRouter = require('./routes/blog');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     name: "passport",
     proxy: true,
@@ -37,7 +38,7 @@ app.use(session({
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/static', express.static(path.join(__dirname,'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -56,33 +57,33 @@ app.use('/blog', blogRouter);
 
 const mongooseOption = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose.connect(process.env.DB_CONNECT || 'mongodb://localhost:27017/admin', mongooseOption, (err) => {
-    if(err)
+    if (err)
         console.log('err');
     console.log('connected to my database');
 });
 
 //REDIRECT WRONG URL.
-app.get('/warning/error', (req, res)=>{
+app.get('/warning/error', (req, res) => {
     res.send('something wrong!');
 });
-app.all('*', function(req, res) {
+app.all('*', function (req, res) {
     res.redirect('/warning/error');
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
-  
-// error handler
-app.use(function(err, req, res, next) {
-// set locals, only providing error in development
-res.locals.message = err.message;
-res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// render the error page
-res.status(err.status || 500);
-res.render('error');
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    // res.render('error');
 });
 
 module.exports = app;
