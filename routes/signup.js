137dd85bafa4fbb1dc617f01042eb962b64e7998
Ho Trung Nhan <hotrungnhan29@gmail.com>
2019-11-18 21:59:41 +0700

    signup chua xong

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+const passport = require("passport");
+const account = require('../controller/userController')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+// router.get('/', function (req, res, next) {
+//   res.send('respond with a resource');
+// });
+// router.post('/',
+//   passport.authenticate('local.signup')
+// );
+// router.post('/',passport.authenticate('local-signup') 
+// );
+router.post('/',
+    passport.authenticate('local-signup'));
 module.exports = router;
