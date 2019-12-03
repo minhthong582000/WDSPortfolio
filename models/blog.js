249876345e3cb0deeb9cbdaa0cd82db1e8d8c08d
lecongnhan <lecongnhan29@gmail.com>
2019-12-03 15:31:14 +0700

    adding comment reply

--- models/blog.js
+++ models/blog.js
@@ -1,34 +1,67 @@
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
-const blog = mongoose.Schema({
+
+
+const blogSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+
+    
     title: {
         require: true,
+
         type: String,
+
         unique: true
     },
+
+
     content: {
         require : true,
+
         type: String,
+
         default : "Content default"
     },
+
+
     date : {
+        require : true,
+
         type: Date,
+
         default : Date.now
     },
+
+
     Auth : {
         type: mongoose.Schema.Types.ObjectId, 
+
         ref: 'user'
     },
+
+
     censorship :{
         type : Boolean,
+
         default : false
     },
+
+    
     comment : [{
         UserComment : {
             type : mongoose.Schema.Types.ObjectId,
+
             ref : "user"
         },
-        contentComment : String
+
+        content : String,
+
+        reply : {
+            type : mongoose.Schema.Types.ObjectId,
+            content : string
+        }
     }]
 })
+
+
+const blog = mongoose.model('blog', blogSchema);
--- services/blogService.js
+++ services/blogService.js
@@ -38,6 +38,7 @@ const commmentBlogByID = async function(_id, _idUserCommnent, content){
     })
 }
 
+
 module.exports = {
     createBlog, 
     removeBlogByFindID, 
