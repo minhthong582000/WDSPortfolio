d04fc8a7e52ea1457197b642a14e8a36c6a0af1d
Ho Trung Nhan <hotrungnhan29@gmail.com>
2019-12-13 22:47:15 +0700

    fix require university

--- controller/signupController.js
+++ controller/signupController.js
@@ -6,23 +6,24 @@ module.exports.createNewAccount = async function (req, email, pass, done) {
         email: Joi.string().email().required(),
         pwd: Joi.string().min(3).max(30).pattern(/^[a-zA-Z0-9]{3,30}$/).required(),
         birthday: Joi.number().integer().min(1900).max(2100),
-        university: Joi.string(),
+        university: Joi.string().required(),
         studentID: Joi.string().required()
     });
     let DTO = req.body;
-    let valid = signUpSchema.validate(DTO, { abortEarly: false });
+    let valid = await signUpSchema.validate(DTO, { abortEarly: false });
     if (valid.error) {
-        return done(null, false, { statusCode: 401, status: 'unauthorized', msg: valid.error.details.map(err=>err.message) });
-    }
-    let user = await userService.userExist(DTO.email, DTO.studentID);
-    if (user) {
-        return done(null, false, { statusCode: 401, status: 'unauthorized', msg: ['Your email or studentID already existed'] });
+        return done(null, false, { statusCode: 401, status: 'unauthorized', msg: await valid.error.details.map(err => err.message) });
     } else {
-        let newUser = await userService.createUser(DTO);
-        if (newUser) {
-            return done(null, newUser, { statusCode: 200, status: 'authorized' });
+        let user = await userService.userExist(DTO.email, DTO.studentID);
+        if (user) {
+            return done(null, false, { statusCode: 401, status: 'unauthorized', msg: ['Your email or studentID already existed'] });
+        } else {
+            let newUser = await userService.createUser(DTO);
+            if (newUser) {
+                return done(null, newUser, { statusCode: 200, status: 'authorized' });
+            }
+            else return done(null, false, { statusCode: 401, status: 'unauthorized', msg: ['Unable to create your account'] });
         }
-        else return done(null, false, { statusCode: 401, status: 'unauthorized', msg: ['Unable to create your account'] });
     }
 };
 
