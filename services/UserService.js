b474460bfdfee30d784bcb8ceb1196085985b472
hotrungnhan <40029631+hotrungnhan@users.noreply.github.com>
2019-12-13 08:32:43 +0700

    Update UserService.js

--- services/UserService.js
+++ services/UserService.js
@@ -24,18 +24,18 @@ const isUserExist = async function (email, studentID) {
         return false;
     }
 }
-const isGGUserExist = async function (googleid) {
-    try {
-        let oldgguser = await usermodel.findOne({ googleid: googleid });
-        if (oldgguser) {
-            return oldgguser;
-        }
-        else return false;
-    }
-    catch (err) {
-        return false;
-    }
-}
+// const isGGUserExist = async function (googleid) {
+//     try {
+//         let oldgguser = await usermodel.findOne({ googleid: googleid });
+//         if (oldgguser) {
+//             return oldgguser;
+//         }
+//         else return false;
+//     }
+//     catch (err) {
+//         return false;
+//     }
+// }
 module.exports = {
     createUser,
     isUserExist,
